ba03585dc49fd85dfafd45d2623465fd2540117e
maul49zer@gmail.com <maul49zer@gmail.com>
2023-04-07 13:39:39 -0600

    done

--- app.js
+++ app.js
@@ -5,15 +5,48 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(`${a} + ${b} = ${add}`);
+console.log(`${a} - ${b} = ${minus}`);
+console.log(`${a} * ${b} = ${multiply}`);
+console.log(`${a} / ${b} = ${dividing}`);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(`num + str = ${num + str}`);
+console.log(`num + str2 = ${num + str2}`);
+console.log(`num + isPresent = ${num + isPresent}`);
+console.log(`firstName + num = ${firstName + num}`);
+console.log(`isPresent + str = ${isPresent + str}`);
+console.log(`firstName + lastName = ${firstName + lastName}`);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
-
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(`the value of: val == str3 is ${val == str3}`);
+console.log(`the value of: val === str3 is ${val === str3}`);
+console.log(`the value of: !isAwake is ${!isAwake}`);
+console.log(`the value of: "eleven" == str4 && val >= str3 is ${"eleven" == str4 && val >= str3}`);
+console.log(`the value of: !isAwake || isAwake is ${!isAwake || isAwake}`);
+console.log(`the value of: 0 == false is ${0 == false}`);
+console.log(`the value of: 0 === false is ${0 === false}`);
+console.log(`the value of: 0 != false is ${val != str3}`);
+console.log(`the value of: 0 !== false is ${0 !== false}`);
